e1d6a17bc36c6e34a459aa36f56e1049948b2c75
DeveloperJulioT <julio.david.1338@gmail.com>
2021-12-12 13:05:32 -0500

    Exercise

---
+++ Como_estuvo_tu_dia.py
@@ -0,0 +1,5 @@
+calificacion = int(input("Como estuvo tu dia (1 al 10): "))
+if calificacion >= 1 and calificacion <= 10:
+    print("Mi dia estuvo: ", calificacion)
+else:
+    print("Ingresa un valor entre 1 a 10")
