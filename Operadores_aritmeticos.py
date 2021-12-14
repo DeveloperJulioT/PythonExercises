63d7f7a9a6cc233b21fbcd751a27d1a7c1a6222c
DeveloperJulioT <julio.david.1338@gmail.com>
2021-12-14 08:57:18 -0500

    execises

--- Operadores_aritmeticos.py
+++ Operadores_aritmeticos.py
@@ -3,3 +3,17 @@ num2 = 3
 suma = num1 + num2
 print(f'La suma es: {suma}')
 
+mult = num1 * num2
+print(f'La Multiplicacion es: {mult}')
+
+Div = num1 / num2
+print(f'La Division es: {Div}')
+
+Div = num1 // num2
+print(f'La Division con resultado int: {Div}')
+
+Mod = num1 % num2
+print(f'El modulo es: ',{Mod})
+
+Pot = num1 ** num2
+print(f'La potencia es: ',{Pot})
---
+++ operadores_comparacion.py
@@ -0,0 +1,20 @@
+a = 2
+b = 4
+
+resultado = a == b
+print(f'Resultado == : {resultado}')
+
+resultado = a != b
+print(f'Resultado != : {resultado}')
+
+resultado = a > b
+print(f'Resultado > : {resultado}')
+
+resultado = a >= b
+print(f'Resultado > : {resultado}')
+
+resultado = a < b
+print(f'Resultado > : {resultado}')
+
+resultado = a <= b
+print(f'Resultado > : {resultado}')
